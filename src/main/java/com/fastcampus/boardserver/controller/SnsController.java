3ffe998b50ccd2d3e49ef7129fcb2ad755704ca0
men1692 <men16922@gmail.com>
2024-03-16 12:16:10 +0900

    슬랙 알림

--- src/main/java/com/fastcampus/boardserver/controller/SnsController.java
+++ src/main/java/com/fastcampus/boardserver/controller/SnsController.java
@@ -1,15 +1,13 @@
 package com.fastcampus.boardserver.controller;
 
 import com.fastcampus.boardserver.config.AWSConfig;
+import com.fastcampus.boardserver.service.SlackService;
 import com.fastcampus.boardserver.service.SnsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.*;
@@ -23,6 +21,7 @@ public class SnsController {
 
     private final AWSConfig awsConfig;
     private final SnsService snsService;
+    private final SlackService slackService;
 
     @PostMapping("/create-topic")
     public ResponseEntity<String> createTopic(@RequestParam final String topicName) {
@@ -79,6 +78,13 @@ public class SnsController {
         return new ResponseStatusException(
                 HttpStatus.INTERNAL_SERVER_ERROR, snsResponse.sdkHttpResponse().statusText().get()
         );
+    }
+
+    // slack
+    @GetMapping("/slack/error")
+    public void error(){
+        log.info("슬랙 error 채널 테스트");
+        slackService.sendSlackMessage("슬랙 에러 테스트", "error");
     }
 
 }
